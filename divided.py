8f0f9183440a74f24dddc29d2200d41b902a2890
Devin <1097456719@qq.com>
2020-03-10 21:29:39 +0800

    分散unified

---
+++ divided.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['CDagency']
+
+unified_col = db['unified'].find()
+
+for item in unified_col:
+    coll_name = item['col_name']
+    db[coll_name].update({'title': item['title']}, {'$set': dict(item)}, True, True)
+
